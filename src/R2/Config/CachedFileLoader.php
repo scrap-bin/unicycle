e2f3c2b066be45d549559f971c4eb469a2db1c8b
artoodetoo <i.am@artoodetoo.org>
2014-10-29 23:03:36 +0600

    minor changes

--- src/R2/Config/CachedFileLoader.php
+++ src/R2/Config/CachedFileLoader.php
@@ -29,7 +29,7 @@ class CachedFileLoader extends SerializedFileLoader
      *
      * @param string $resource
      *
-     * @return Boolean
+     * @return boolean
      */
     public function supports($resource)
     {
